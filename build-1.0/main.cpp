d08cadfa4adceb11eb486646f715453793fa0d35
Otavio Andre <otavioandre111@gmail.com>
2023-05-24 22:52:01 -0300

    criação da funcão responsavel pela primeira derivação

--- build-1.0/main.cpp
+++ build-1.0/main.cpp
@@ -3,13 +3,48 @@ using namespace std;
 
 void derivadas()
 {
-    
-    float p_termo, termo_der, exp, g_exp; // criação das variaveis primeiro termo, termo derivado, expoente, guardar-expoente;
-    char acomp_x;
+
+    float p_termo = 0, termo_der = 0, exp = 0, g_exp = 0, termo_der_2 = 0; // criação das variaveis primeiro termo, termo derivado, expoente, guardar-expoente;
+    char acomp_x, check_der_2;
     cout << "Seu termo vem acompanhado de X? y | n: ";
     cin >> acomp_x;
 
+    cout << "Diga o termo a ser derivado em X: ";
+    cin >> p_termo;
 
+    cout << "Diga o expoente que acompanha o primeiro termo: ";
+    cin >> exp;
+
+    if (acomp_x == 'y' && exp != 1)
+    {
+        termo_der = (p_termo * exp);
+        g_exp = exp;
+        exp = exp - 1;
+        cout << "A derivada de " << p_termo << "X^" << g_exp << " eh igual a: " << termo_der << "X^" << exp << "." << endl;
+    }
+    else if (exp == 1)
+    {
+        cout << "A derivada de" << p_termo << " em relacao a X eh igual a: " <<p_termo << endl;
+    }
+    else if (acomp_x == 'n')
+    {
+        cout << "A derivada de" << p_termo << " em relacao a X eh igual a: 0" << endl;
+    }
+
+    cout << "Deseja derivar novamente? y | n: ";
+    cin >> check_der_2;
+
+    if (check_der_2 == 'y' && acomp_x == 'y') // Realizar a criação de um laço WHILE para reduzir linhas e maior otimização.
+    {
+        termo_der_2 = (termo_der * exp);
+        g_exp = exp;
+        exp = exp - 1;
+        cout << "A derivada de " << termo_der << "X^" << g_exp << " eh igual a: " << termo_der_2 << "X^" << exp << "." << endl;
+    }
+    else if (check_der_2 == 'y' && acomp_x == 'n')
+    {
+        cout << "O fato do resultado da primeira derivada ter sido igual a 0, tambem torna a segunda derivada igual a 0" << endl;
+    }
 }
 
 int main()
@@ -22,6 +57,12 @@ int main()
 
     if (check == 'y')
     {
-        derivadas(); // chamando a func derivada se check = y
+        do
+        {
+            derivadas(); // chamando a func derivada se check = y
+            cout << "Deseja calcular derivada com um novo termo? y | n: ";
+            cin >> check;
+
+        } while (check == 'y');
     }
 }
